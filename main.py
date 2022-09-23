52b55357f07c7134e4e3bedafa27b944e69cb23a
Julien Fraichot <julien.fraichot@hyland.com>
2022-09-23 22:09:24 +0200

    get energy reading

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ print("configuration: " + "tp110 ip - " + CONFIG.tp_110_ip)
 
 p110 = PyP110.P110(CONFIG.tp_110_ip, CONFIG.user_email, CONFIG.user_password) #Creating a P110 plug object
 
+print(p110)
+
 p110.handshake() #Creates the cookies required for further methods
 p110.login() #Sends credentials to the plug and creates AES Key and IV for further methods
 
-#PyP110 has all PyP100 functions and additionally allows to query energy usage infos
+# #PyP110 has all PyP100 functions and additionally allows to query energy usage infos
 energyValue = p110.getEnergyUsage() #Returns dict with all the energy usage
 print(energyValue)
