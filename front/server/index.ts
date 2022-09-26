68259ca6b2921464179da481858d11fd79e5b0b1
Julien Fraichot <julien.fraichot@hyland.com>
2022-09-26 22:02:44 +0200

    add BFF

---
+++ front/server/index.ts
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const DATA_FILE = './data/daily_consumption.json'
+
+app.get('/read', function (req, res) {
+  const data = fs.readFileSync(DATA_FILE, { encoding:'utf8' });
+  console.log('read request received', new Date(Date.now()).toISOString());
+  res.send(data);
+});
+
+app.listen('3333', function () {
+  console.log('server started on port 3333');
+});
